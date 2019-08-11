fe6cefc68180e89a6715b4272f4226d8a309bab8
Richard Hosking <richardh@iinet.net.au>
2019-08-11 12:52:34 +0930

    Add files via upload

---
+++ ADF435x.cpp
@@ -0,0 +1,354 @@
+/* 
+   ADF435x.cpp - ADF435x PLL Communication Library
+   Created by Neal Pisenti, 2013.
+   JQI - Strontium - UMD
+ Substantially modified by Steve Pettitt, SEC Ltd 2016
+ Modified further by Richard Hosking 
+
+  */
+
+#include "ADF435x.h" 
+
+  
+// Initializes a new ADF435x object referenceClock (in MHz), and initial frequency Hz/10 to fit in 2^32
+
+void ADF435x::initialize(uint32_t frequency, uint32_t referenceClock){
+    _freq = frequency; // one tenth of real value REMEMBER!
+    _refClk = referenceClock;
+    phase = 1;
+
+    _feedbackType = 0;  //0=fractional, 1=integer
+    if (_feedbackType) _ABP = 1;
+    _doubler = 0; //
+    _halve = 0; //
+    _channelSpacing = 25000; //25KHz
+    _powerdown = 0;
+    _auxOnOff = 0;
+    _rfEnabled = 1;
+    _prescaler = 1;  // 4/5 or 8/9...
+
+    // default power = -5dbm or 0 in this case... (0-3)
+    _auxPower = 0;
+    _rfPower = 0;
+
+    // sets register values which don't have dynamic values...
+    ADF435x::setR1();
+    ADF435x::setR3();
+    ADF435x::setR5();
+
+    ADF435x::setFreq(_freq);
+    
+
+    ADF435x::update();
+}
+
+// gets current frequency setting
+int ADF435x::getFreq(){
+    return _freq;
+}
+// set up for approx 40 MHz out temporary
+void ADF435x::setFreq(uint32_t freq){
+    R_counter = 250; // Divide 25 MHz to 100 KHz for channel spacing
+    _divider = 6; // divide main output by 64 for low freq out 
+    _mod = 1562; // 25 MHz clock 100KHz channel spacing 
+    _frac = 101; // reduce with fractional N to 1KHz channel spacing 
+    _integ = 29921; //Ref Freq 100KHz VCO range 2-4.5GHz 
+}
+
+/*void ADF435x::setFreq(uint32_t freq){
+// frequency passed is one tenth real value to stay within 32-bit value
+    _freq = freq;
+    int multiplier;
+    // selects the right divider range (ie, output divider is 2^(_divider))
+    // the multiplier is required if you're using fundamental feedback to N-counter
+    if (_freq > 220000000) {
+        _divider = 0;
+        multiplier = 1;
+        R_counter = 50;
+    }
+    else if (_freq > 110000000) {
+        _divider = 1;               // divide output by 2 
+        multiplier = 2;
+        R_counter = 25;
+        }
+        else if (_freq > 55000000) {
+            _divider = 2;            // divide output by 4 
+            multiplier = 4;
+            R_counter = 16;
+            }
+            else if (_freq > 27500000) {
+                    _divider = 3;          // divide output by 8  
+                    multiplier = 8;
+                    R_counter = 8;
+                }
+                else if (_freq >13750000) {
+                        _divider = 4;               // divide output by 16 
+                        multiplier = 16;
+                        R_counter = 4;
+                    }
+                    else if (_freq >6875000) {
+                            _divider = 5;               // divide output by 32 
+                            multiplier = 32;
+                            R_counter = 2;
+                        }
+                        else {  //_freq lies between 34.375MHz to 68.75MHz
+                            _divider = 6;               // divide output by 64 
+                            multiplier = 64;
+                            R_counter = 1;
+                        }
+ 
+  
+
+        _mod = _refClk/_channelSpacing; // 10000000/100 = 1000000
+        _mod *= _doubler; //_doubler should always be set
+        _mod >>= _divider;
+        _mod /= R_counter;
+ 
+        while (_mod < 4096) {
+            if(!_mod){_mod++;} // if _mod is zero loop will hang 
+            _mod *=2; // set mod as close to 4095 as possible...
+        }
+        _mod /= 2; //restore to less than 4096
+
+    uint64_t test = _freq*10;
+    test <<= _divider;
+    test *= R_counter;
+    uint32_t tast = _refClk << _doubler;
+    tast >>= _halve;
+    _integ = (uint64_t)(test/tast);
+    _frac = ((uint64_t)(test % tast)*_mod)/(int32_t)tast;
+    
+    // reduce the frac/mod fraction....
+    uint32_t x=1, y, z;
+    y = _mod; z = _frac;
+    while (x != 0) {
+        if (y>z) {
+            x = y - z;
+        }
+        else x = z - y;
+        y = z;
+        z = x;
+    }
+    _mod /= y; _frac /= y;
+   
+
+}
+*/
+// updates dynamic registers, and writes values to PLL board
+void ADF435x::update(){
+    ADF435x::setR0();
+    ADF435x::setR1();
+    ADF435x::setR2();
+    ADF435x::setR3();
+    ADF435x::setR4();
+    ADF435x::setR5();
+    
+    digitalWrite(ChipEnable,LOW);
+
+    // writes registers to device
+    ADF435x::programRegister(R5.reg5);
+    ADF435x::programRegister(R4.reg4);
+    ADF435x::programRegister(R3.reg3);
+    ADF435x::programRegister(R2.reg2);
+    ADF435x::programRegister(R1.reg1);
+    ADF435x::programRegister(R0.reg0);
+    
+    digitalWrite(ChipEnable,HIGH);
+}
+
+void ADF435x::setFeedbackType(bool feedback){
+    _feedbackType = feedback;
+}
+
+void ADF435x::powerDown(bool pd){
+    _powerdown = pd;
+    ADF435x::setR2();
+    ADF435x::update();
+
+}
+
+void ADF435x::rfEnable(bool rf){
+    _rfEnabled = rf;
+    ADF435x::setR4();
+    ADF435x::update();
+}
+
+// CAREFUL!!!! pow must be 0, 1, 2, or 3... corresponding to -4, -1, 3, 5 dbm.
+void ADF435x::setRfPower(int pow){
+    _rfPower = pow;
+    ADF435x::setR4();
+    ADF435x::update();
+}
+
+void ADF435x::auxEnable(void){
+    _auxOnOff = 1;
+    ADF435x::setR4();
+    ADF435x::write4Bytes(R4.reg4);
+}
+
+void ADF435x::auxDisable(void){
+    _auxOnOff = 0;
+    ADF435x::setR4();
+    ADF435x::write4Bytes(R4.reg4);
+//    ADF435x::update();
+}
+
+// CAREFUL!!!! pow must be 0, 1, 2, or 3... corresponding to -4, -1, 3, 5 dbm.
+void ADF435x::setAuxPower(int pow){
+    _auxPower = pow;
+    ADF435x::setR4();
+    ADF435x::update();
+}
+
+// REGISTER UPDATE FUNCTIONS
+
+void ADF435x::setR0(){
+    R0.d0.reserved = 0;
+    R0.d0.integer = _integ; // not less than 23 decimal
+    R0.d0.frac = _frac;
+    R0.d0.ctrl = 0;
+}
+
+void ADF435x::setR1(){
+    R1.d1.reserved = 0;
+    R1.d1.phaseAdjust = 0; // disabled
+    R1.d1.prescaler = 1; //  4/5 or 8/9?
+    R1.d1.phaseValue = 1; //recommended value
+    R1.d1.modulus = _mod;
+    R1.d1.ctrl = 1;
+}
+
+void ADF435x::setR2(){
+    R2.d2.reserved = 0;
+    R2.d2.noiseMode = 3;
+    R2.d2.Muxout = 6;
+    R2.d2.refDoubler = _doubler;
+    R2.d2.refDivBy2 = _halve;
+    R2.d2.rCounter = R_counter;  
+    R2.d2.doubleBuffer = 0;
+    R2.d2.chargePump = 7;
+    R2.d2.LDF = _feedbackType; //fractional(0) or integer(1)?
+    R2.d2.LDP = 0; //10nS (or 6nS if 1)
+    R2.d2.PD_Polarity = 1;// 1 for noninverting or passive filter
+    R2.d2.PD = 0;
+    R2.d2.CP3state = 0;
+    R2.d2.counterReset = 0;
+    R2.d2.ctrl = 2;
+}
+
+void ADF435x::setR3(){
+    R3.d3.reserved1 = 0;
+    R3.d3.bandSelCkMode = 0;
+    R3.d3.ABP = _ABP;
+    R3.d3.chargeCancel = 0;
+    R3.d3.reserved2 = 0;
+    R3.d3.CSR = 0;
+    R3.d3.reserved3 = 0;
+    R3.d3.clockDivMode = 0b10;
+    R3.d3.clockDivider = 1000; //divide by 1000 25Khz steps
+    R3.d3.ctrl = 3;
+}
+
+void ADF435x::setR4(){
+    R4.d4.reserved = 0;
+    R4.d4.feedbackSel = 1;          //0= divided, 1= fundamental
+    R4.d4.RF_div = _divider;
+    R4.d4.bandSelCkDiv = 160;
+    R4.d4.VCO_PD = 0;               // VCO powered up
+    R4.d4.MTLD = 0;                 // Mute til lock disabled
+    R4.d4.auxOutEnable = _auxOnOff;
+    R4.d4.auxOutSel = 0;            //0 divided 1 fundamental
+    R4.d4.auxOutPower = _auxPower;
+    R4.d4.RF_OutEn = 1;
+    R4.d4.OutputPower = _rfPower;
+    R4.d4.ctrl = 4;
+
+ }
+
+void ADF435x::setR5(){
+    R5.d5.reserved1 = 0;
+    R5.d5.LD_Mode = 0b11; //lock detect mode pin goes high with lock
+    R5.d5.reserved2 = 0;
+    R5.d5.reserved3 = 0b11;
+    R5.d5.reserved4 = 0;
+    R5.d5.ctrl = 0b101;
+}
+
+
+// Program one 32 bit register on the ADF4351
+// data bits 0-2 specify which register
+// Note for double buffered data R0 must be written to again to clock in data 
+
+void ADF435x::programRegister(uint32_t registerData) {
+    
+    pinMode(ChipEnable, OUTPUT);  // Setup port
+    pinMode(LoadEnable, OUTPUT);
+    pinMode(DataClock, OUTPUT);
+    pinMode(LockDetect, INPUT);
+    pinMode(MultiplexData, INPUT);
+    pinMode(SerialData, OUTPUT);
+
+    
+    digitalWrite(LoadEnable, LOW);
+    digitalWrite(DataClock, LOW);
+    digitalWrite(SerialData, LOW);  // Initialize all data lines 
+    bool bitdata = 0;
+    uint32_t data = registerData;
+    
+  for (int i = 31; i >= 0; i--) {
+        bitdata = bitRead(data, i); // Get each bit of register data, starting with MSB
+    
+        digitalWrite(SerialData, bitdata);  // and write to ADF4351
+        //delay(10);
+        digitalWrite(DataClock, HIGH);
+        //delay(10);
+        digitalWrite(DataClock, LOW);
+        //delay(10);
+  }
+     
+  digitalWrite(LoadEnable, HIGH);
+         //delay(10);
+  digitalWrite(LoadEnable, LOW);
+         //delay(10);
+  
+}
+
+
+// debugging only
+void ADF435x::debug(){
+    Serial.print("\nFrequency(KHz) = ");Serial.println(_freq);
+    //Serial.print("fPFD = ");Serial.println(tast);
+    Serial.print("Integer = ");Serial.println(_integ);
+    Serial.print("Modulus = ");Serial.println(_mod);
+    Serial.print("Fraction = ");Serial.println(_frac);
+    Serial.print("R Counter = ");Serial.println(R_counter);
+    Serial.print("Divider = ");Serial.println(_divider);
+    //Serial.print("Multiplier = ");Serial.println(multiplier);
+    Serial.print("Channel Spacing = ");Serial.println(_channelSpacing);
+    Serial.print("Reference Clock = ");Serial.println(_refClk);
+    Serial.print("R0 = ");
+    Serial.println(R0.reg0,HEX);
+    Serial.print("R0 = ");
+    Serial.println(R0.reg0,BIN);
+    Serial.print("R1 = ");
+    Serial.println(R1.reg1,HEX);
+    Serial.print("R1 = ");
+    Serial.println(R1.reg1,BIN);
+    Serial.print("R2 = ");
+    Serial.println(R2.reg2,HEX);
+    Serial.print("R2 = ");
+    Serial.println(R2.reg2,BIN);
+    Serial.print("R3 = ");
+    Serial.println(R3.reg3,HEX);
+    Serial.print("R3 = ");
+    Serial.println(R3.reg3,BIN);
+    Serial.print("R4 = ");
+    Serial.println(R4.reg4,HEX); 
+    Serial.print("R4 = ");
+    Serial.println(R4.reg4,BIN);
+    Serial.print("R5 = ");
+    Serial.println(R5.reg5,HEX);
+    Serial.print("R5 = ");
+    Serial.println(R5.reg5,BIN);
+}
+
---
+++ ADF435x.h
@@ -0,0 +1,180 @@
+/* 
+   ADF435x.h - ADF435x PLL Communication Library
+   Created by Neal Pisenti, 2013.
+   JQI - Strontium - UMD
+
+    Massively modified for Energia - Steve Pettitt, SEC 2016
+    Modified further by Richard Hosking 
+  */
+
+#ifndef ADF435x_h
+#define ADF435x_h
+
+#include "Arduino.h"
+
+class ADF435x{
+    
+    public: 
+ 
+        // Initialize with initial frequency, refClk (defaults to 10Mhz); 
+        void initialize(uint32_t, uint32_t );
+
+
+        // Functions to turn on and off the PLL/VCO
+        void powerDown(bool);
+        void setRfPower(int);
+        void setAuxPower(int);
+        void auxEnable(void);
+        void auxDisable(void);
+        void rfEnable(bool);
+
+        // Gets current frequency
+        int getFreq();
+
+        // Sets frequency
+        void setFreq(uint32_t);
+
+        void setFeedbackType(bool);
+
+        // Update the device 
+        void update();
+        void programRegister(uint32_t registerData); 
+        
+       //Debugging 
+        void debug();
+        
+        // Variables 
+        uint32_t _freq, fPFD, _refClk, _integ;
+        int phase;
+        unsigned int _divider, _auxPower, _rfPower;
+        uint32_t _mod;
+        unsigned int _frac, _modulus, _channelSpacing, _outputDivider, _fPDF, R_counter;
+    
+        // Port pin assignments
+    int ChipEnable = 14;
+    int LoadEnable = 5;
+    int DataClock = 3;
+    int SerialData = 4;
+    int LockDetect = 15;
+    int MultiplexData = 6; 
+    
+    
+    // function to write data to register.
+    void setR0();
+    void setR1();
+    void setR2();
+    void setR3();
+    void setR4();
+    void setR5();
+    void writeRegister(byte[4]);
+    void write4Bytes(uint32_t);
+    
+    private:
+         // Instance variables for ...
+    bool _powerdown, _auxOnOff, _rfEnabled, _feedbackType, _prescaler, _ABP;
+    unsigned int _doubler, _halve;
+
+    
+    typedef struct R0bitfield{
+        unsigned int ctrl:3;
+        unsigned int frac:12;       // Fractional N divider 12 bits 
+        unsigned int integer:16;    // Integer divider 16 bits 
+        unsigned int reserved:1;
+    } d0;
+    union {
+        uint32_t reg0;
+        R0bitfield d0;
+    } R0;
+    
+    
+    typedef struct R1bitfield{
+        unsigned int ctrl:3;
+        unsigned int modulus:12;
+        unsigned int phaseValue:12;
+        unsigned int prescaler:1;      // Prescaler 4/5 or 8/9
+        unsigned int phaseAdjust:1;
+        unsigned int reserved:3;
+    } d1;
+    union {
+        uint32_t reg1;
+        R1bitfield d1;
+    } R1;
+    
+    typedef struct R2bitfield{
+        unsigned int ctrl:3;
+        unsigned int counterReset:1;
+        unsigned int CP3state:1;    // Charge pump 3 state 0 for normal operation 
+        unsigned int PD:1;          // Power down if 1 reset at startup
+        unsigned int PD_Polarity:1; // Phase detector polarity 1 for noninverting filter 
+        unsigned int LDP:1;         // Lock detect precision - window 
+        unsigned int LDF:1;         // O = Frac N 1 = Integer cycles 
+        unsigned int chargePump:4;     // Set charge pump current according to loop filter design 
+        unsigned int doubleBuffer:1;  // Double buffer RF divider select in R4 if 1 
+        unsigned int rCounter:10;   // Reference counter - sets channel; spacing 
+        unsigned int refDivBy2:1;   // Extend Ref in frequency
+        unsigned int refDoubler:1;  // Enable reference doubler 
+        unsigned int Muxout:3;      // Select multiplex output 
+        unsigned int noiseMode:2;   // Low noise or low spur (dither enabled) 
+        unsigned int reserved:1;
+    } d2;
+    union {
+        uint32_t reg2;
+        R2bitfield d2;
+    } R2;
+    
+    
+    typedef struct R3bitfield{
+        unsigned int ctrl:3;
+        unsigned int clockDivider:12;
+        unsigned int clockDivMode:2;
+        unsigned int reserved3:1;
+        unsigned int CSR:1;
+        unsigned int reserved2:2;
+        unsigned int chargeCancel:1;
+        unsigned int ABP:1;
+        unsigned int bandSelCkMode:1;
+        unsigned int reserved1:8;
+    } d3;
+    union {
+        uint32_t reg3;
+        R3bitfield d3;
+    } R3;
+    
+    typedef struct R4bitfield{
+        unsigned int ctrl:3;
+        unsigned int OutputPower:2;
+        unsigned int RF_OutEn:1;
+        unsigned int auxOutPower:2;
+        unsigned int auxOutEnable:1;
+        unsigned int auxOutSel:1;
+        unsigned int MTLD:1;
+        unsigned int VCO_PD:1;
+        unsigned int bandSelCkDiv:8;
+        unsigned int RF_div:3;
+        unsigned int feedbackSel:1;
+        unsigned int reserved:8;
+    } d4;
+    union {
+        uint32_t reg4;
+        R4bitfield d4;
+    } R4;
+    
+    typedef struct R5bitfield{
+        unsigned int ctrl:3;
+        unsigned int reserved4:16;
+        unsigned int reserved3:2;
+        unsigned int reserved2:1;
+        unsigned int LD_Mode:2;
+        unsigned int reserved1:8;
+    } d5;
+    union {
+        uint32_t reg5;
+        R5bitfield d5;
+    } R5;
+    
+        // register values
+
+};
+ 
+
+#endif
